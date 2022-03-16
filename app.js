699be8156e6e69895dd2ae3e60915d4a8d3e359f
Himanshu Shekhar <shekharh433@gmail.com>
2022-03-16 18:13:54 +0530

    Uplode weather project

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const bodyParse = require('body-parser');
+
+app.use(bodyParse.urlencoded({extends:true}));
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/index.html");
+
+    //res.send("No Data Available!");
+});
+
+app.post("/",function(req,res){
+    //console.log( req.body );
+
+    var city = req.body.city;
+    const state = req.body.state;
+    const country = req.body.country;
+    const apiKey = "efdd1e7c3361e11fcd22b76dbcae441a";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+","+state+","+country+"&APPID="+apiKey+"&units=metric";
+
+    https.get(url,function(response){
+        //console.log(response);
+
+        response.on('data',function(data){
+            // console.log(data);
+
+            //city = "mo";
+            const weatherData=JSON.parse(data);
+            // console.log(weatherData);
+
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const main = weatherData.weather[0].main;
+
+            const iconId = weatherData.weather[0].icon;
+            const imgSrc = "http://openweathermap.org/img/wn/"+iconId+"@2x.png";
+
+            res.write("<p>The weather in "+city+" is currently "+description+"</p>");
+            console.log(city);
+
+            res.write("<h1>Temp : "+temp+" celcius "+", Main : "+main+"</h1>");
+
+            res.write("<img src="+imgSrc+"></img>");
+
+            res.send();
+        });
+
+    });
+
+});
+
+app.listen(3000,function(){
+    console.log("Your server is at : localhost:3000");
+});
